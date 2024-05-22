0f9557303e45489210c998256646e425507d69bb
hamza walker <hamzawalker.vienna@gmail.com>
2024-05-22 08:58:37 +0200

    add CustomeHero to _Hero directory. created the index.tsx and index.module.scss files      in components => hero => index.ts add the custome hero to the heros object      fields => richText => hero.ts add custome hero to hero field and to condition      pages => slug => pages.tsx add custome hero to to homepage.      add a custome hero and style it abit in the admin pannal

--- src/payload/payload-types.ts
+++ src/payload/payload-types.ts
@@ -45,7 +45,7 @@ export interface Page {
   title: string;
   publishedOn?: string | null;
   hero: {
-    type: 'none' | 'highImpact' | 'mediumImpact' | 'lowImpact';
+    type: 'none' | 'highImpact' | 'mediumImpact' | 'lowImpact' | 'customeHero';
     richText: {
       [k: string]: unknown;
     }[];
@@ -187,6 +187,8 @@ export interface Media {
   filesize?: number | null;
   width?: number | null;
   height?: number | null;
+  focalX?: number | null;
+  focalY?: number | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
