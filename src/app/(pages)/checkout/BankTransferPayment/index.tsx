5b99337a9bbee3babd73488eba26d545b810bf88
hamza walker <hamzawalker.vienna@gmail.com>
2024-07-07 14:21:59 +0200

    added a component for promotionalCodeInput. it should accept a code and deisplay a checksignal if its valid also apply to the disount to the checkout price

--- src/app/(pages)/checkout/BankTransferPayment/index.tsx
+++ src/app/(pages)/checkout/BankTransferPayment/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PromoCodeInput from '../PromoCodeInput/index'
 
 import classes from './index.module.scss'
 const BankTransferPayment: React.FC = () => {
@@ -6,10 +7,17 @@ const BankTransferPayment: React.FC = () => {
     <div>
       <h3 className={classes.payment}>Bank Transfer Details</h3>
       <p>Please transfer the total amount to the following bank account:</p>
-      <p>Bank: XYZ Bank</p>
+      <p>Bank: BAWAG</p>
+      <p>IBAN: AT39 60000 0104 1019 7559</p>
       <p>Account Number: 123456789</p>
       <p>Sort Code: 00-00-00</p>
+      <PromoCodeInput />
     </div>
   )
 }
 export default BankTransferPayment
+
+// TODO: add terms and conditions checkbox
+// TODO: create a function that will send an email after the user confirms the orders
+// TODO: add button to redicrect to a successful processing of the order
+// TODO: add Promo code field
---
+++ src/app/(pages)/checkout/PromoCodeInput/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+
+const PromoCodeInput: React.FC = () => {
+  return (
+    <div className={classes.promoCodeInput}>
+      <h3>Redeem promotional coupon</h3>
+      <p>Enter your promotional code below:</p>
+      <input type="text" placeholder="Promotional Code" />
+    </div>
+  )
+}
+
+export default PromoCodeInput
