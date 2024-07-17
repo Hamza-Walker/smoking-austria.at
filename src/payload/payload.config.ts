0c14f74e47ebfe5184c9440596db15b5e1aec8eb
hamza walker <hamzawalker.vienna@gmail.com>
2024-07-17 05:52:58 +0200

    added cors and csrf to domain

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,32 +1,31 @@
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import nestedDocs from '@payloadcms/plugin-nested-docs'
-import redirects from '@payloadcms/plugin-redirects'
-import seo from '@payloadcms/plugin-seo'
-import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
-import stripePlugin from '@payloadcms/plugin-stripe'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import dotenv from 'dotenv'
-import path from 'path'
-import { buildConfig } from 'payload/config'
-
+import BeforeDashboard from './components/BeforeDashboard'
+import BeforeLogin from './components/BeforeLogin'
 import Categories from './collections/Categories'
+import { Footer } from './globals/Footer'
+import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
+import { Header } from './globals/Header'
 import { Media } from './collections/Media'
 import { Orders } from './collections/Orders'
 import { Pages } from './collections/Pages'
 import Products from './collections/Products'
+import { Settings } from './globals/Settings'
 import Users from './collections/Users'
-import BeforeDashboard from './components/BeforeDashboard'
-import BeforeLogin from './components/BeforeLogin'
+import { buildConfig } from 'payload/config'
 import { createPaymentIntent } from './endpoints/create-payment-intent'
 import { customersProxy } from './endpoints/customers'
-import { productsProxy } from './endpoints/products'
-import { Footer } from './globals/Footer'
-import { Header } from './globals/Header'
-import { Settings } from './globals/Settings'
+import dotenv from 'dotenv'
+import nestedDocs from '@payloadcms/plugin-nested-docs'
+import path from 'path'
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { priceUpdated } from './stripe/webhooks/priceUpdated'
 import { productUpdated } from './stripe/webhooks/productUpdated'
+import { productsProxy } from './endpoints/products'
+import redirects from '@payloadcms/plugin-redirects'
+import seo from '@payloadcms/plugin-seo'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import stripePlugin from '@payloadcms/plugin-stripe'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
 
 const generateTitle: GenerateTitle = () => {
   return 'My Store'
@@ -88,12 +87,19 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  cors: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
-    Boolean,
-  ),
-  csrf: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
-    Boolean,
-  ),
+  cors: [
+    'https://checkout.stripe.com',
+    'http://smoking-austria.at',
+    'http://www.smoking-austria.at',
+    ,
+    process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
+  ].filter(Boolean),
+  csrf: [
+    'https://checkout.stripe.com',
+    'http://smoking-austria.at',
+    'http://www.smoking-austria.at',
+    process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
+  ].filter(Boolean),
   endpoints: [
     {
       path: '/create-payment-intent',
