66637e44a7486f76744ad16364014fdffb117e9f
hamza walker <hamzawalker.vienna@gmail.com>
2024-07-29 16:47:06 +0200

    no build errors

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,43 +1,74 @@
-import BeforeDashboard from './components/BeforeDashboard'
-import BeforeLogin from './components/BeforeLogin'
-import Categories from './collections/Categories'
-import { Footer } from './globals/Footer'
-import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
-import { Header } from './globals/Header'
-import { Media } from './collections/Media'
-import { Orders } from './collections/Orders'
-import { Pages } from './collections/Pages'
-import Products from './collections/Products'
-import { Settings } from './globals/Settings'
-import Users from './collections/Users'
-import { buildConfig } from 'payload/config'
-import { createPaymentIntent } from './endpoints/create-payment-intent'
-import { customersProxy } from './endpoints/customers'
-import dotenv from 'dotenv' // Correct import for dotenv
-import { merge } from 'webpack-merge'
-import nestedDocs from '@payloadcms/plugin-nested-docs'
-import path from 'path'
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { priceUpdated } from './stripe/webhooks/priceUpdated'
-import { productUpdated } from './stripe/webhooks/productUpdated'
-import { productsProxy } from './endpoints/products'
-import redirects from '@payloadcms/plugin-redirects'
-import { seed } from './endpoints/seed'
-import seo from '@payloadcms/plugin-seo'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import stripePlugin from '@payloadcms/plugin-stripe'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-
-const customWebpackConfig = require('../../webpack.config')
+import BeforeDashboard from './components/BeforeDashboard';
+import BeforeLogin from './components/BeforeLogin';
+import Categories from './collections/Categories';
+import { Footer } from './globals/Footer';
+import type { GenerateTitle } from '@payloadcms/plugin-seo/types';
+import { Header } from './globals/Header';
+import { Media } from './collections/Media';
+import { Orders } from './collections/Orders';
+import { Pages } from './collections/Pages';
+import Products from './collections/Products';
+import { Settings } from './globals/Settings';
+import Users from './collections/Users';
+import type { Configuration as WebpackConfig } from 'webpack';
+import { buildConfig } from 'payload/config';
+import { createPaymentIntent } from './endpoints/create-payment-intent';
+import { customersProxy } from './endpoints/customers';
+import dotenv from 'dotenv'; // Correct import for dotenv
+import { merge } from 'webpack-merge';
+import nestedDocs from '@payloadcms/plugin-nested-docs';
+import path from 'path';
+import { payloadCloud } from '@payloadcms/plugin-cloud';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { priceUpdated } from './stripe/webhooks/priceUpdated';
+import { productUpdated } from './stripe/webhooks/productUpdated';
+import { productsProxy } from './endpoints/products';
+import redirects from '@payloadcms/plugin-redirects';
+import { seed } from './endpoints/seed';
+import seo from '@payloadcms/plugin-seo';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import stripePlugin from '@payloadcms/plugin-stripe';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
 
 const generateTitle: GenerateTitle = () => {
-  return 'My Store'
-}
+  return 'My Store';
+};
 
 dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
-})
+});
+
+const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js');
+
+const customWebpackConfig: WebpackConfig = {
+  resolve: {
+    fallback: {
+      assert: require.resolve('assert/'),
+      url: require.resolve('url/'),
+      os: require.resolve('os-browserify/browser'),
+      fs: false,
+      child_process: false,
+    },
+    alias: {
+      dotenv: path.resolve(__dirname, './dotenv.js'),
+      [path.resolve(__dirname, 'collections/Products/hooks/beforeChange')]: mockModulePath,
+      [path.resolve(__dirname, 'collections/Users/hooks/createStripeCustomer')]: mockModulePath,
+      [path.resolve(__dirname, 'collections/Users/endpoints/customer')]: mockModulePath,
+      [path.resolve(__dirname, 'endpoints/create-payment-intent')]: mockModulePath,
+      [path.resolve(__dirname, 'endpoints/customers')]: mockModulePath,
+      [path.resolve(__dirname, 'endpoints/products')]: mockModulePath,
+      [path.resolve(__dirname, 'endpoints/seed')]: mockModulePath,
+      stripe: mockModulePath,
+      express: mockModulePath,
+    },
+  },
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
+};
 
 export default buildConfig({
   admin: {
@@ -64,12 +95,8 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  cors: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
-    Boolean,
-  ),
-  csrf: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
-    Boolean,
-  ),
+  cors: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
+  csrf: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
   endpoints: [
     {
       path: '/create-payment-intent',
