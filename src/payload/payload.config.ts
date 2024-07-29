8a99d3134842675a7b34ed8714c64291568ae32b
hamza walker <hamzawalker.vienna@gmail.com>
2024-07-29 16:00:39 +0200

    fixed webpack errors

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -13,7 +13,8 @@ import Users from './collections/Users'
 import { buildConfig } from 'payload/config'
 import { createPaymentIntent } from './endpoints/create-payment-intent'
 import { customersProxy } from './endpoints/customers'
-import dotenv from 'dotenv'
+import dotenv from './dotenv' // Correct import for dotenv
+import { merge } from 'webpack-merge'
 import nestedDocs from '@payloadcms/plugin-nested-docs'
 import path from 'path'
 import { payloadCloud } from '@payloadcms/plugin-cloud'
@@ -22,30 +23,17 @@ import { priceUpdated } from './stripe/webhooks/priceUpdated'
 import { productUpdated } from './stripe/webhooks/productUpdated'
 import { productsProxy } from './endpoints/products'
 import redirects from '@payloadcms/plugin-redirects'
+import { seed } from './endpoints/seed'
 import seo from '@payloadcms/plugin-seo'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import stripePlugin from '@payloadcms/plugin-stripe'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3'
-import { cloudStorage } from '@payloadcms/plugin-cloud-storage'
+
+const customWebpackConfig = require('../../webpack.config')
+
 const generateTitle: GenerateTitle = () => {
   return 'My Store'
 }
-
-const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js')
-
-const storageAdapter = s3Adapter({
-  config: {
-    endpoint: process.env.S3_ENDPOINT,
-    region: process.env.S3_REGION,
-    forcePathStyle: false,
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_KEY,
-    },
-  },
-  bucket: process.env.S3_BUCKET_NAME,
-})
 
 dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
@@ -56,35 +44,10 @@ export default buildConfig({
     user: Users.slug,
     bundler: webpackBundler(),
     components: {
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
       beforeLogin: [BeforeLogin],
-      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: [BeforeDashboard],
     },
-    webpack: config => {
-      return {
-        ...config,
-        resolve: {
-          ...config.resolve,
-          alias: {
-            ...config.resolve?.alias,
-            dotenv: path.resolve(__dirname, './dotenv.js'),
-            [path.resolve(__dirname, 'collections/Products/hooks/beforeChange')]: mockModulePath,
-            [path.resolve(__dirname, 'collections/Users/hooks/createStripeCustomer')]:
-              mockModulePath,
-            [path.resolve(__dirname, 'collections/Users/endpoints/customer')]: mockModulePath,
-            [path.resolve(__dirname, 'endpoints/create-payment-intent')]: mockModulePath,
-            [path.resolve(__dirname, 'endpoints/customers')]: mockModulePath,
-            [path.resolve(__dirname, 'endpoints/products')]: mockModulePath,
-            [path.resolve(__dirname, 'endpoints/seed')]: mockModulePath,
-            stripe: mockModulePath,
-            express: mockModulePath,
-          },
-        },
-      }
-    },
+    webpack: config => merge(config, customWebpackConfig),
   },
   editor: slateEditor({}),
   db: postgresAdapter({
@@ -92,7 +55,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI,
     },
   }),
-  serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   collections: [Pages, Products, Orders, Media, Categories, Users],
   globals: [Settings, Header, Footer],
   typescript: {
@@ -101,19 +64,12 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  cors: [
-    'https://checkout.stripe.com',
-    'http://smoking-austria.at',
-    'http://www.smoking-austria.at',
-    ,
-    process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
-  ].filter(Boolean),
-  csrf: [
-    'https://checkout.stripe.com',
-    'http://smoking-austria.at',
-    'http://www.smoking-austria.at',
-    process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
-  ].filter(Boolean),
+  cors: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
+    Boolean,
+  ),
+  csrf: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
+    Boolean,
+  ),
   endpoints: [
     {
       path: '/create-payment-intent',
@@ -130,8 +86,11 @@ export default buildConfig({
       method: 'get',
       handler: productsProxy,
     },
-    // The seed endpoint is used to populate the database with some example data
-    // You should delete this endpoint before deploying your site to production
+    {
+      path: '/seed',
+      method: 'get',
+      handler: seed,
+    },
   ],
   plugins: [
     stripePlugin({
@@ -157,13 +116,5 @@ export default buildConfig({
       uploadsCollection: 'media',
     }),
     payloadCloud(),
-
-    cloudStorage({
-      collections: {
-        media: {
-          adapter: storageAdapter,
-        },
-      },
-    }),
   ],
 })
