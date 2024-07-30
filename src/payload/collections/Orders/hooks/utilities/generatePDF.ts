daa94ba1d0876d086f1fd7180523068848134b2b
hamza walker <hamzawalker.vienna@gmail.com>
2024-07-30 12:41:10 +0200

    added another flag to the generatePDF function --disablegpu. it should assis in the deployed environment

--- src/payload/collections/Orders/hooks/utilities/generatePDF.ts
+++ src/payload/collections/Orders/hooks/utilities/generatePDF.ts
@@ -3,16 +3,15 @@ import htmlPdfNode from 'html-pdf-node'
 
 export const generatePDF = async (html: string, outputPath: string): Promise<string> => {
   const file = { content: html }
-  const options = { format: 'A4' }
+  const options = {
+    format: 'A4',
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'], // Add --disable-gpu here
+    executablePath: '/usr/bin/chromium-browser', // Update this path if needed
+  }
 
   return new Promise<string>((resolve, reject) => {
     htmlPdfNode
-      .generatePdf(file, {
-        format: 'A4',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/usr/bin/chromium-browser',
-        options,
-      })
+      .generatePdf(file, options)
       .then((pdfBuffer: Buffer) => {
         fs.writeFile(outputPath, pdfBuffer, err => {
           if (err) {
