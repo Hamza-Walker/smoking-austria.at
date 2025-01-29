68dc01c41c5d7628ba9b6ba9a562a5602f05a0db
Hamza <global@Hamzas-MacBook-Pro.local>
2025-01-29 16:47:48 +0100

    removed the promotion for now

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -62,7 +62,7 @@ export default async function Page({ params: { slug = 'home' } }) {
     <React.Fragment>
       {slug === 'home' ? (
         <section>
-          <Promotion />
+          {/* <Promotion /> */}
           <MediaCarousel mediaBlocks={layout} />
 
           <Gutter className={classes.home}>
